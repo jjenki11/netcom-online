d9b198a38acaa6fce95027725e5185392dc92a65
Chris <christopher.frenchi@gmail.com>
2016-02-15 18:33:47 -0500

    server.c changes

--- src/server.c
+++ src/server.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
      int sockfd, newsockfd, portno;
      socklen_t clilen;
      char buffer[256];
+     scanf("%s", command);
      struct sockaddr_in serv_addr, cli_addr;
      int n;
      if (argc < 2) {
@@ -26,7 +27,7 @@ int main(int argc, char *argv[])
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
@@ -34,22 +35,30 @@ int main(int argc, char *argv[])
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+     do
+     {
+         listen(sockfd,5);
+         clilen = sizeof(cli_addr);
+         newsockfd = accept(sockfd,
+                     (struct sockaddr *) &cli_addr,
+                     &clilen);
+         if (newsockfd < 0)
+              error("ERROR on accept");
+         bzero(buffer,256);
+         n = read(newsockfd,buffer,255);
+         if (n < 0) error("ERROR reading from socket");
+         printf("Here is the message: %s\n",buffer);
+         n = write(newsockfd,"I got your message",18);
+         if (n < 0) error("ERROR writing to socket");
+         if (strcmp( buffer, "exit") == 0 )
+         {
+            printf("\nClosing Connection!\n");
+            close(newsockfd);
+            close(sockfd);
+         }
+
+     }while(n!=0);
+     return 0;
 }
